0643d8769427c711f11bf077ef7c2beebfbd6411
sarenvs <52559685+sarenvs@users.noreply.github.com>
2019-07-15 23:12:22 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,18 +1,18 @@
 
-AA,BB=map(int,input().split())
-CC=list(map(int,input().split()))
+A,B=map(int,input().split())
+C=list(map(int,input().split()))
 pr=list(map(int,input().split()))
 qr=[]
 ar=0
-for i in range(AA):
-    x=pr[i]/CC[i]
+for i in range(A):
+    x=pr[i]/C[i]
     qr.append(x)
-while BB>=0 and len(qr)>0:
+while B>=0 and len(qr)>0:
     mindex=qr.index(max(qr))
-    if BB>=CC[mindex]:
+    if B>=C[mindex]:
         ar=ar+pr[mindex]
-        BB=BB-CC[mindex]
-    CC.pop(mindex)
+        B=B-C[mindex]
+    C.pop(mindex)
     pr.pop(mindex)
     qr.pop(mindex)
 print(ar)
