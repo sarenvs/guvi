9adf475c35ca233af058cbb56dffa7a251239655
sarenvs <52559685+sarenvs@users.noreply.github.com>
2019-07-15 22:12:18 +0530

    Update indexsum.py

--- indexsum.py
+++ indexsum.py
@@ -3,6 +3,6 @@ a=list(map(int,input().split(None,x)[:x]))
 f=[]
 for i in range(y):
     d,g=map(int,input().split())
-    f.append(sum(a[d:g+1]))
+    f.append(sum(a[d-1:g]))
 for i in f:
     print(i)
