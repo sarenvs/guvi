af431f911eaf5ab0fd3e96baa7aad740a77db036
sarenvs <52559685+sarenvs@users.noreply.github.com>
2019-07-17 18:15:36 +0530

    Update reversestring.py

--- reversestring.py
+++ reversestring.py
@@ -1,10 +1,13 @@
 a=input()
 a=a.split(" ")
 
+
 b=""
 for i in a:
     if i!="":
        v=list(i)
        v=v[::-1]
-       b=b+"".join(v)+" "
+       b=b+"".join(v)
+       b=b+" "      
+    
 print(b)
